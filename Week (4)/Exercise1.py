0ced12e004b8750124f7c0c50862971bee8f11cc
Lee Van Hiep <daihiep2k@gmail.com>
2019-11-13 13:57:00 +0700

    Update Exercise1.py

--- Week (4)/Exercise1.py
+++ Week (4)/Exercise1.py
@@ -2,3 +2,6 @@ print('Moi ban nhap cac phan tu vao mang A: ')
 A = list(map(int,input(),split()))
 print('Moi ban nhap cac phan tu vao mang B: ')
 B = list(map(int,input(),split()))
+print('Mang tong cua A va B la: ')
+for i in range(len(A)):
+	print(A[i]+B[i],end=' ')
